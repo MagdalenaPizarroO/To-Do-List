6be8a99cc2cb85b1e231e639afdc55497633b186
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-03-24 20:48:50 +0000

    trabajando con la Maca clase 23 marzo min 12:50

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-import Applist from "./toDoList.jsx";
+import React, {useState, useEffect} from "react";
+
 
 
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState([]);
+	const url =  'https://assets.breatheco.de/apis/fake/todos/user/magdis'
+	
+	useEffect(()=>{
+	//fetch api
+	fetch(url)
+	.then(response=>response.json())
+	.then(data=>setTareas(data))},[])
+	
+
+
 	return (
-		<Applist/>
+		<div className="text-center">
+			<h1>ToDo List API</h1>
+			{tareas.map((elem, index)=>{
+				return <li>{elem?.label}</li>
+
+			})}
+		</div>
 	);
 };
 
